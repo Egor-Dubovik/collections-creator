6ed28ba09744c3dcc225e544a094edc1b65fb8a9
Egor Dubovik <egorka54896@gmail.com>
2023-07-20 23:35:33 +0300

    refactor: add next to errorMiddleware

--- server/src/middleware/errorMiddleware.ts
+++ server/src/middleware/errorMiddleware.ts
@@ -1,8 +1,8 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { errorMessage } from '../common/constant/error';
 import ApiError from '../exceptions/ApiError';
 
-export const errorMiddleware = (err: Error, req: Request, res: Response) => {
+export const errorMiddleware = (err: Error, req: Request, res: Response, next: NextFunction) => {
 	return err instanceof ApiError
 		? res.status(err.status).json({ message: err.message, errors: err.errors })
 		: res.status(500).json({ message: errorMessage.unexpected });
