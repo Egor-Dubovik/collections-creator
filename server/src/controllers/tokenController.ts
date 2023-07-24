c21e893119a8f89a5f35dd32e6436186839f3c7b
Egor Dubovik <egorka54896@gmail.com>
2023-07-24 10:41:21 +0300

    refactor: change refresh token, throw params (server)

--- server/src/controllers/tokenController.ts
+++ server/src/controllers/tokenController.ts
@@ -1,12 +1,16 @@
 import { NextFunction, Request, Response } from 'express';
+import { errorMessage } from '../common/constant/error';
+import ApiError from '../exceptions/ApiError';
 import tokenService from '../services/tokenService';
 import { setRefreshToken } from '../utils/cookie';
 
 class TokenController {
 	async refresh(req: Request, res: Response, next: NextFunction) {
 		try {
-			const { refreshToken } = req.cookies;
-			const userData = await tokenService.refresh(refreshToken);
+			// const { refreshToken } = req.cookies;
+			const { refreshToken } = req.query;
+			if (!refreshToken) return next(ApiError.badRequest(errorMessage.notAuthorized));
+			const userData = await tokenService.refresh(refreshToken as string);
 			setRefreshToken(res, userData.refreshToken);
 			return res.json(userData);
 		} catch (err) {
